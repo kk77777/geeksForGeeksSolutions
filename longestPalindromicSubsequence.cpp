d156861a1229b30269317ccccaba6bc0df8715fb
kk77777 <kaushikgattani@gmail.com>
2020-04-02 13:09:38 +0530

    Longest Palindromic Subsequence using bottom up approach

---
+++ longestPalindromicSubsequence.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t[205][205];
+
+int lps(string x, string y, int m, int n)
+{
+
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                t[i][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (x[i - 1] == y[j - 1])
+            {
+                t[i][j] = 1 + t[i - 1][j - 1];
+            }
+            else
+            {
+                t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+            }
+        }
+    }
+
+    return t[m][n];
+}
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        memset(t, -1, sizeof t);
+        string x, y;
+        int m, n;
+        cin >> x;
+        y = x;
+        reverse(y.begin(), y.end());
+        m = x.length();
+        n = y.length();
+
+        cout << lps(x, y, m, n) << "\n";
+    }
+}
