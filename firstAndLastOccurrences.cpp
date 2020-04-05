1dc476c94bbf0a9a5890ee5e7a14b8030e300356
kk77777 <kaushikgattani@gmail.com>
2020-04-05 10:27:34 +0530

    First and last occurrences of X

---
+++ firstAndLastOccurrences.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int ans = -1;
+int first(int a[], int f, int l, int x)
+{
+
+    if (f <= l)
+    {
+        int mid = f + (l - f) / 2;
+        if (a[mid] == x)
+        {
+            ans = mid;
+            return first(a, f, mid - 1, x);
+        }
+        else if (a[mid] > x)
+        {
+            return first(a, f, mid - 1, x);
+        }
+        else
+        {
+            return first(a, mid + 1, l, x);
+        }
+    }
+    return ans;
+}
+
+int last(int a[], int f, int l, int x)
+{
+    if (f <= l)
+    {
+        int mid = f + (l - f) / 2;
+        if (a[mid] == x)
+        {
+            ans = mid;
+            return last(a, mid + 1, l, x);
+        }
+        else if (a[mid] > x)
+        {
+            return last(a, f, mid - 1, x);
+        }
+        else
+        {
+            return last(a, mid + 1, l, x);
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n, x;
+        cin >> n >> x;
+        int a[n + 1];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int res = first(a, 0, n - 1, x);
+        if (res == -1)
+        {
+            cout << -1 << "\n";
+        }
+        else
+        {
+            cout << res << " " << last(a, 0, n - 1, x) << "\n";
+        }
+    }
+}
