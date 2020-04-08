b00accdc093013efeba2fd4fa6ff93213e1c58b7
kk77777 <kaushikgattani@gmail.com>
2020-04-08 18:01:15 +0530

    Next larger element

---
+++ nextLargerElement.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+void solve(vector<ll> &a, ll n)
+{
+    vector<ll> v;
+    stack<ll> s;
+
+    for (ll i = n - 1; i >= 0; i--)
+    {
+        if (s.empty())
+        {
+            v.push_back(-1);
+        }
+
+        else
+        {
+            while (!s.empty() && s.top() <= a[i])
+            {
+                s.pop();
+            }
+            if (!s.empty())
+            {
+                v.push_back(s.top());
+            }
+            else
+            {
+                v.push_back(-1);
+            }
+        }
+        s.push(a[i]);
+    }
+
+    reverse(v.begin(), v.end());
+    for (ll i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> a;
+        for (ll i = 0; i < n; i++)
+        {
+            ll x;
+            cin >> x;
+            a.push_back(x);
+        }
+        solve(a, n);
+    }
+}
