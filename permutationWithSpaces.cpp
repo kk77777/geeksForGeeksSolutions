b5a450b1069e21cd43cf915e538e5e2d32e72a83
kk77777 <kaushikgattani@gmail.com>
2020-07-08 11:44:58 +0530

    Permutation with Spaces

---
+++ permutationWithSpaces.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(string ip, string op)
+{
+    if (ip.length() == 0)
+    {
+        cout << "(" << op << ")";
+        return;
+    }
+
+    string op1 = op, op2 = op;
+    op1.push_back(' ');
+    op1.push_back(ip[0]);
+    op2.push_back(ip[0]);
+    ip.erase(ip.begin() + 0);
+    solve(ip, op1);
+    solve(ip, op2);
+    return;
+}
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        string ip;
+        cin >> ip;
+        string op = "";
+        op.push_back(ip[0]);
+        ip.erase(ip.begin() + 0);
+        solve(ip, op);
+        cout << "\n";
+    }
+}
