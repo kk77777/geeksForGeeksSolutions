7273605cf143644b921c30ff66f9030338d08909
kk77777 <kaushikgattani@gmail.com>
2021-01-24 14:48:16 +0530

    Merge two sorted linked lists (using Recursion with extra space)

--- mergeTwoSortedLinkedList.cpp
+++ mergeTwoSortedLinkedList.cpp
@@ -1,21 +1,100 @@
-Node *sortedMerge(Node *head1, Node *head2)
-{
-    // Your Code Here
-    if (head1 == NULL)
-        return head2;
-    if (head2 == NULL)
-        return head1;
+// { Driver Code Starts
+#include <iostream>
+using namespace std;
 
-    Node *head;
-    if (head1->data < head2->data)
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
     {
-        head = head1;
-        head->next = sortedMerge(head1->next, head2);
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *sortedMerge(struct Node *a, struct Node *b);
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *n)
+{
+    while (n != NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        int data;
+        cin >> data;
+        struct Node *head1 = new Node(data);
+        struct Node *tail1 = head1;
+        for (int i = 1; i < n; ++i)
+        {
+            cin >> data;
+            tail1->next = new Node(data);
+            tail1 = tail1->next;
+        }
+
+        cin >> data;
+        struct Node *head2 = new Node(data);
+        struct Node *tail2 = head2;
+        for (int i = 1; i < m; i++)
+        {
+            cin >> data;
+            tail2->next = new Node(data);
+            tail2 = tail2->next;
+        }
+
+        Node *head = sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+  
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+Node *sortedMerge(Node *a, Node *b)
+{
+    // code here
+    Node *res = NULL;
+    if (a == NULL)
+        return b;
+    if (b == NULL)
+        return a;
+    if (a->data < b->data)
+    {
+        res = a;
+        res->next = sortedMerge(a->next, b);
     }
     else
     {
-        head = head2;
-        head->next = sortedMerge(head1, head2->next);
+        res = b;
+        res->next = sortedMerge(a, b->next);
     }
-    return head;
+    return res;
 }
