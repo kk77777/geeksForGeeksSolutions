325f0eba4e072fd778967ee82bdc192be65590a1
kk77777 <kaushikgattani@gmail.com>
2021-01-25 15:40:18 +0530

    Intersection Point in Y Shapped Linked Lists

--- intersectionPointInYShappedLinkedLists.cpp
+++ intersectionPointInYShappedLinkedLists.cpp
@@ -1,45 +1,124 @@
-int intersectPoint(Node *head1, Node *head2)
+// { Driver Code Starts
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+int intersectPoint(struct Node *head1, struct Node *head2);
+
+Node *inputList(int size)
+{
+    if (size == 0)
+        return NULL;
+
+    int val;
+    cin >> val;
+
+    Node *head = new Node(val);
+    Node *tail = head;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        cin >> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T, n1, n2, n3;
+
+    cin >> T;
+    while (T--)
+    {
+        cin >> n1 >> n2 >> n3;
+
+        Node *head1 = inputList(n1);
+        Node *head2 = inputList(n2);
+        Node *common = inputList(n3);
+
+        Node *temp = head1;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL)
+            temp->next = common;
+
+        temp = head2;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL)
+            temp->next = common;
+
+        cout << intersectPoint(head1, head2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+/* Should return data of intersection point of two linked
+   lists head1 and head2.
+   If there is no intersecting point, then return -1. */
+int intersectPoint(Node *a, Node *b)
 {
     // Your Code Here
-    Node *a = head1;
-    Node *b = head2;
-
-    int l1 = 0, l2 = 0;
-
-    while (a->next != NULL)
+    Node *d1 = a, *d2 = b;
+    int la = 0, lb = 0;
+    while (d1 != NULL)
     {
-        l1++;
-        a = a->next;
+        la++;
+        d1 = d1->next;
     }
-    while (b->next != NULL)
+    d1 = a;
+    while (d2 != NULL)
     {
-        l2++;
-        b = b->next;
+        lb++;
+        d2 = d2->next;
     }
-    int diff = abs(l1 - l2);
-    Node *m = head1;
-    Node *n = head2;
-    if (l1 > l2)
+    d2 = b;
+    // cout << "la = " << la << "  lb = " << lb << "\n";
+    if (la > lb)
     {
-        for (int i = 0; i < diff; i++)
-        {
-            m = m->next;
-        }
+        swap(la, lb);
+        swap(d1, d2);
     }
-    else
+    int x = lb - la;
+    while (x)
     {
-        for (int i = 0; i < diff; i++)
-        {
-            n = n->next;
-        }
+        d2 = d2->next;
+        x--;
     }
-    while (m != NULL && n != NULL)
+    while (d1 != NULL && d2 != NULL)
     {
-        if (m == n)
-        {
-            return m->data;
-        }
-        m = m->next;
-        n = n->next;
+        if (d1 == d2)
+            return d1->data;
+        d1 = d1->next;
+        d2 = d2->next;
     }
+    return -1;
 }
