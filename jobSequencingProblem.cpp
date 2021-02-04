2fd3010df76b876a151225010eb58afad4e50a23
kk77777 <kaushikgattani@gmail.com>
2021-02-04 09:21:49 +0530

    Job Sequencing Problem

---
+++ jobSequencingProblem.cpp
@@ -0,0 +1,84 @@
+// { Driver Code Starts
+// Program to find the maximum profit job sequence from a given array
+// of jobs with deadlines and profits
+#include <bits/stdc++.h>
+using namespace std;
+
+// A structure to represent a job
+struct Job
+{
+    int id;     // Job Id
+    int dead;   // Deadline of job
+    int profit; // Profit if job is over before or on deadline
+};
+
+// } Driver Code Ends
+
+// Prints minimum number of platforms reqquired
+
+bool comp(Job a, Job b)
+{
+    return a.profit > b.profit;
+}
+
+pair<int, int> JobScheduling(Job arr[], int n)
+{
+    // your code here
+    sort(arr, arr + n, comp);
+    int mx = arr[0].dead;
+    for (int i = 1; i < n; i++)
+    {
+        mx = max(arr[i].dead, mx);
+    }
+    vector<int> v(mx + 1, -1);
+    int cnt = 0, profit = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = arr[i].dead; j > 0; j--)
+        {
+            if (v[j] == -1)
+            {
+                v[j] = i;
+                cnt++;
+                profit += arr[i].profit;
+                break;
+            }
+        }
+    }
+
+    return {cnt, profit};
+}
+
+// { Driver Code Starts.
+// Driver program to test methods
+int main()
+{
+    int t;
+    //testcases
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+
+        //size of array
+        cin >> n;
+        Job arr[n];
+
+        //adding id, deadline, profit
+        for (int i = 0; i < n; i++)
+        {
+            int x, y, z;
+            cin >> x >> y >> z;
+            arr[i].id = x;
+            arr[i].dead = y;
+            arr[i].profit = z;
+        }
+        //function call
+        pair<int, int> ans = JobScheduling(arr, n);
+        cout << ans.first << " " << ans.second << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
