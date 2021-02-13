9fe2faa018281a19039c63948dd38c3ad9dbf835
kk77777 <kaushikgattani@gmail.com>
2021-02-13 21:30:20 +0530

    Power of 2]

--- powerOf2.cpp
+++ powerOf2.cpp
@@ -1,29 +1,45 @@
-#include <iostream>
-#include <cmath>
-#define ll long long int
+// Problem link : https://practice.geeksforgeeks.org/problems/power-of-2-1587115620/1
+// { Driver Code Starts
+//Initial Template for C++
 
+#include <bits/stdc++.h>
 using namespace std;
 
-bool check(ll n)
+// } Driver Code Ends
+
+//User function Template for C++
+
+// Function to check if given number n is a power of two.
+bool isPowerofTwo(long long n)
 {
+
+    // Your code here
     if (n == 0)
-        return false;
-    ll m = n - 1;
-    ll res = (n & m);
-    if (res)
-        return false;
+        return 0;
     else
-        return true;
+        return !(n & (n - 1));
 }
 
+// { Driver Code Starts.
+
+// Driver code
 int main()
 {
-    int tc;
-    cin >> tc;
-    while (tc--)
+
+    int t;
+    cin >> t; //testcases
+
+    for (int i = 0; i < t; i++)
     {
-        ll n;
+        long long n; //input a number n
         cin >> n;
-        check(n) ? cout << "YES\n" : cout << "NO\n";
+
+        if (isPowerofTwo(n)) //Now, if log2 produces an integer not decimal then we are sure raising 2 to this value
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
     }
+
+    return 0;
 }
+// } Driver Code Ends
