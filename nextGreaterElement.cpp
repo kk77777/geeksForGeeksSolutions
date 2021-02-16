ff198f158ab01a93c44a7605044767941141831a
kk77777 <kaushikgattani@gmail.com>
2021-02-16 15:29:41 +0530

    Next Greater Element

---
+++ nextGreaterElement.cpp
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+// Function to return Next larger elements
+
+vector<long long> nextLargerElement(vector<long long> arr, int n)
+{
+    // Your code here
+    stack<long long> s;
+    vector<long long> ans;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (s.empty())
+        {
+            ans.push_back(-1);
+        }
+        else
+        {
+            while (!s.empty() && arr[i] >= s.top())
+            {
+                s.pop();
+            }
+            if (!s.empty())
+            {
+                ans.push_back(s.top());
+            }
+            else
+            {
+                ans.push_back(-1);
+            }
+        }
+        s.push(arr[i]);
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+        vector<long long> arr(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        vector<long long> res = nextLargerElement(arr, n);
+        for (long long i : res)
+            cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
