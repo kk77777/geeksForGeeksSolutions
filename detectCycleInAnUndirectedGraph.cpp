28d907c1fe359fe742980c14045e5a5ea93487f7
kk77777 <kaushikgattani@gmail.com>
2021-05-17 15:48:09 +0530

    Detect cycle in an undirected graph (using DFS)

--- detectCycleInAnUndirectedGraph.cpp
+++ detectCycleInAnUndirectedGraph.cpp
@@ -4,75 +4,66 @@ using namespace std;
 
 // } Driver Code Ends
 
-/* This function is used to detect a cycle in undirected graph
-
-*  g[]: array of vectors to represent graph
-*  V: number of vertices
-*/
-bool dfs(vector<int> g[], vector<bool> &vis, int src)
+class Solution
 {
-    vis[src] = true;
-    queue<pair<int, int>> q;
-    q.push({src, -1});
-    while (!q.empty())
+public:
+    //Function to detect cycle in an undirected graph.
+
+    bool checkCycle(int x, int parent, vector<int> adj[], vector<bool> &vis)
     {
-        pair<int, int> temp = q.front();
-        q.pop();
-        for (auto i : g[temp.first])
+        vis[x] = true;
+        for (auto i : adj[x])
         {
-            // cout<<"i = "<<i<<"\n";
-            if (vis[i] && i != temp.second)
-                return true;
-            else if (!vis[i])
+            if (!vis[i])
             {
-                q.push({i, temp.first});
-                vis[i] = true;
+                if (checkCycle(i, x, adj, vis))
+                    return true;
+            }
+            else if (i != parent)
+                return true;
+        }
+        return false;
+    }
+
+    bool isCycle(int n, vector<int> adj[])
+    {
+        // Code here
+        vector<bool> vis(n + 1, false);
+        for (int i = 0; i < n; i++)
+        {
+            if (!vis[i])
+            {
+                if (checkCycle(i, -1, adj, vis))
+                    return true;
             }
         }
+        return false;
     }
-    return false;
-}
-
-bool isCyclic(vector<int> g[], int V)
-{
-    // Your code here
-    vector<bool> vis(V, 0);
-    for (int i = 0; i < V; i++)
-    {
-        if (!vis[i])
-        {
-            if (dfs(g, vis, i))
-                return true;
-        }
-    }
-    return false;
-}
+};
 
 // { Driver Code Starts.
-
 int main()
 {
-    int T;
-    cin >> T;
-    while (T--)
+    int tc;
+    cin >> tc;
+    while (tc--)
     {
         int V, E;
         cin >> V >> E;
-
-        // array of vectors to represent graph
         vector<int> adj[V];
-
-        int u, v;
         for (int i = 0; i < E; i++)
         {
+            int u, v;
             cin >> u >> v;
-
-            // adding edge to the graph
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-
-        cout << isCyclic(adj, V) << endl;
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
     }
-}
-// } Driver Code Ends
+    return 0;
+} // } Driver Code Ends
