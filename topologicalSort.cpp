210e3d4872c72e887c0560bff5442cfcafe15e30
kk77777 <kaushikgattani@gmail.com>
2021-05-18 14:15:19 +0530

    Topological sort (using DFS)

--- topologicalSort.cpp
+++ topologicalSort.cpp
@@ -1,35 +1,96 @@
-void topsrt(int i, bool vis[], vector<int> adj[], stack<int> &s)
-{
-    vis[i] = true;
-    for (int j = 0; j < adj[i].size(); j++)
-    {
-        if (!vis[adj[i][j]])
-        {
-            topsrt(adj[i][j], vis, adj, s);
-        }
-    }
-    s.push(i);
-}
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
 
-int *topoSort(int n, vector<int> adj[])
+// } Driver Code Ends
+
+class Solution
 {
 
-    bool vis[n] = {0};
-    stack<int> s;
-    for (int i = 0; i < n; i++)
+    void dfs(int x, vector<int> adj[], vector<bool> &vis, stack<int> &s)
     {
-        if (!vis[i])
+        vis[x] = 1;
+        for (auto i : adj[x])
         {
-            topsrt(i, vis, adj, s);
+            if (!vis[i])
+            {
+                dfs(i, adj, vis, s);
+            }
+        }
+        s.push(x);
+    }
+
+public:
+    //Function to return list containing vertices in Topological order.
+    vector<int> topoSort(int V, vector<int> adj[])
+    {
+        // code here
+        vector<int> ans;
+        stack<int> s;
+        vector<bool> vis(V + 1, 0);
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+                dfs(i, adj, vis, s);
+        }
+        while (!s.empty())
+        {
+            ans.push_back(s.top());
+            s.pop();
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector<int> &res, vector<int> adj[])
+{
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++)
+    {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++)
+    {
+        for (int v : adj[i])
+        {
+            if (map[i] > map[v])
+                return 0;
         }
     }
-    int *a = new int[n];
-    int k = 0;
-    while (!s.empty())
-    {
-        a[k] = s.top();
-        s.pop();
-        k++;
-    }
-    return a;
+    return 1;
 }
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++)
+        {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        Solution obj;
+        vector<int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+
+    return 0;
+} // } Driver Code Ends
