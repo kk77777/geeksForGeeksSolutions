32183c15eadd1b224219f913ce54e125a64ff227
kk77777 <kaushikgattani@gmail.com>
2021-05-20 01:46:23 +0530

    Strongly Connected Components (Kosaraju\'s Algo)

--- stronglyConnectedComponents_KosarajusAlgo.cpp
+++ stronglyConnectedComponents_KosarajusAlgo.cpp
@@ -1,60 +1,99 @@
-void dfs(int x, vector<int> rg[], bool vis[])
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
 {
-    vis[x] = 1;
-    for (auto i = rg[x].begin(); i != rg[x].end(); i++)
+
+    void toposort(int x, stack<int> &s, vector<bool> &vis, vector<int> adj[])
     {
-        if (!vis[*i])
+        vis[x] = 1;
+        for (auto it : adj[x])
         {
-            dfs(*i, rg, vis);
+            if (!vis[it])
+                toposort(it, s, vis, adj);
+        }
+        s.push(x);
+    }
+
+    void revDfs(int x, vector<bool> &vis, vector<int> transpose[])
+    {
+        vis[x] = 1;
+        for (auto it : transpose[x])
+        {
+            if (!vis[it])
+                revDfs(it, vis, transpose);
         }
     }
+
+public:
+    //Function to find number of strongly connected components in the graph.
+
+    int kosaraju(int V, vector<int> adj[])
+    {
+        //code here
+        stack<int> s;
+        vector<bool> vis(V + 1, 0);
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                toposort(i, s, vis, adj);
+            }
+        }
+        vector<int> transpose[V];
+        for (int i = 0; i < V; i++)
+        {
+            vis[i] = 0;
+            for (auto it : adj[i])
+            {
+                transpose[it].push_back(i);
+            }
+        }
+
+        int cnt = 0;
+        while (!s.empty())
+        {
+            int temp = s.top();
+            s.pop();
+            if (!vis[temp])
+            {
+                cnt++;
+                revDfs(temp, vis, transpose);
+            }
+        }
+        return cnt;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        Solution obj;
+        cout << obj.kosaraju(V, adj) << "\n";
+    }
+
+    return 0;
 }
 
-void tsort(vector<int> adj[], bool vis[], stack<int> &s, int x)
-{
-    vis[x] = 1;
-    for (auto i = adj[x].begin(); i < adj[x].end(); i++)
-    {
-        if (vis[*i] == 0)
-        {
-            tsort(adj, vis, s, *i);
-        }
-    }
-    s.push(x);
-}
-
-int kosaraju(int V, vector<int> adj[])
-{
-    bool vis[V] = {0};
-    stack<int> s;
-    for (int i = 0; i < V; i++)
-    {
-        if (!vis[i])
-        {
-            tsort(adj, vis, s, i);
-        }
-    }
-    vector<int> rg[V];
-    for (int i = 0; i < V; i++)
-    {
-        for (auto j = adj[i].begin(); j != adj[i].end(); j++)
-        {
-            rg[*j].push_back(i);
-        }
-    }
-    for (int i = 0; i < V; i++)
-    {
-        vis[i] = 0;
-    }
-    int scc = 0;
-    while (!s.empty())
-    {
-        int t = s.top();
-        s.pop();
-        if (!vis[t])
-        {
-            dfs(t, rg, vis);
-            scc++;
-        }
-    }
-    return
+// } Driver Code Ends
