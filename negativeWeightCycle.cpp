019244311154778f1f61b1cb6845616b5b4d6df7
kk77777 <kaushikgattani@gmail.com>
2021-05-20 20:03:31 +0530

    Negative weight cycle

---
+++ negativeWeightCycle.cpp
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int checkUsingBellmanFord(int n, vector<vector<int>> &edge, int source)
+    {
+        int m = edge.size();
+        vector<int> dist(n + 1, INT_MAX);
+        for (int i = 0; i < n; i++)
+        {
+            dist[i] = INT_MAX;
+        }
+        dist[source] = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                int u = edge[j][0];
+                int v = edge[j][1];
+                int wt = edge[j][2];
+                if (dist[u] != INT_MAX && dist[u] + wt < dist[v])
+                {
+                    dist[v] = dist[u] + wt;
+                }
+            }
+        }
+        for (int j = 0; j < m; j++)
+        {
+            int u = edge[j][0];
+            int v = edge[j][1];
+            int wt = edge[j][2];
+            if (dist[u] != INT_MAX && dist[u] + wt < dist[v])
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    int isNegativeWeightCycle(int n, vector<vector<int>> edges)
+    {
+        // Code here
+        return checkUsingBellmanFord(n, edges, 0);
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> edges;
+        for (int i = 0; i < m; i++)
+        {
+            int x, y, z;
+            cin >> x >> y >> z;
+            edges.push_back({x, y, z});
+        }
+        Solution obj;
+        int ans = obj.isNegativeWeightCycle(n, edges);
+        cout << ans << "\n";
+    }
+    return 0;
+} // } Driver Code Ends
