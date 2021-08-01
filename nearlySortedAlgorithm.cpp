4b51dd9699550b55b68766c6e22ff5cb9b205a7c
kk77777 <kaushikgattani@gmail.com>
2021-08-01 14:07:08 +0530

    Nearly Sorted Algorithm

--- nearlySortedAlgorithm.cpp
+++ nearlySortedAlgorithm.cpp
@@ -1,39 +1,43 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include<queue>
 using namespace std;
 
-int main()
-{
-    int tc;
-    cin >> tc;
-    while (tc--)
-    {
-        int n, k;
-        cin >> n >> k;
-        int a[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
+class Solution{
+    public:
+    void nearlySort(int arr[],int n,int k){
+        int s=n==k?k:k+1;
+        priority_queue<int,vector<int>,greater<int>>pq(arr,arr+s);
+        int index=0;
+        for(int i=k+1;i<n;i++){
+            arr[index++]=pq.top();
+            pq.pop();
+            pq.push(arr[i]);
         }
-        priority_queue<int, vector<int>, greater<int>> mh;
-        vector<int> v;
-        for (int i = 0; i < n; i++)
-        {
-            mh.push(a[i]);
-            if (mh.size() > k)
-            {
-                v.push_back(mh.top());
-                mh.pop();
-            }
+        while(!pq.empty()){
+            arr[index++]=pq.top();
+            pq.pop();
         }
-        while (mh.size() > 0)
-        {
-            v.push_back(mh.top());
-            mh.pop();
-        }
-        for (int i = 0; i < v.size(); i++)
-        {
-            cout << v[i] << " ";
-        }
-        cout << "\n";
     }
+};
+
+
+int main() {
+	//code
+	int tc;
+	cin>>tc;
+	while(tc--){
+	    int n,k;
+	    cin>>n>>k;
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    Solution obj;
+	    obj.nearlySort(arr,n,k);
+	    for(int i=0;i<n;i++){
+	        cout<<arr[i]<<" ";
+	    }
+	    cout<<"\n";
+	}
+	return 0;
 }
